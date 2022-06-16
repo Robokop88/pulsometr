8ee75f0bf7ca280a1a8758a163e4047ad483cfc4
Dima <salaev334@gmail.com>
2022-06-16 22:58:31 +0300

    slide

---
+++ js/script.js
@@ -0,0 +1,42 @@
+/* $(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 700,
+        slidesToShow: 1,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/pulsometer/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/pulsometer/right.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            }
+        ]
+      });
+}); */
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  speed: 200,
+  controls: false,
+  responsive: {
+    991: {
+      items: 1,
+      autoplay: true,
+      autoplayTimeout: 400,
+    }
+  }
+});
+
+document.querySelector('.prev').addEventListener('click', function() {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+  slider.goTo('next');
+});
